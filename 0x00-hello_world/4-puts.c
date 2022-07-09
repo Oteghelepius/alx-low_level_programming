a50001168f55584b95539b39aeb29b64d737782d
oteghelepius <otegjelepius@gmail.com>
2022-07-09 02:52:00 -0700

    c-hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/
- * main - Entry point
- *  * Return: Always 0 (sucess)
- *   */
+/**
+ * main - entry point
+ *
+ * Return: 0 after printing tge function
+ */
  int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
+
+{puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 :
