091795da2f32eb5f2a597a0b3a98af62918b0f2c
oteghelepius <otegjelepius@gmail.com>
2022-07-09 19:50:01 -0700

    c-fumctions

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+*main - entry point
+*
+* Return: always returns 0
+*/
+int main(void)
+{
+int i;
+for (i = 10; i < 20; i++)
+{
+putchar((i % 10) + '0');
+{
+putchar(',');
+putchar(' ');
+}
+}
+putxhar('\n');
+return (0);
+}
